23c3506f2c851110b357f9c0407ce962671892d4
Michael J. Sullivan <sully@msully.net>
2018-09-13 12:03:23 -0700

    sigh

--- scripts/build-osx-wheels.sh
+++ scripts/build-osx-wheels.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -eux
 
 set -e
-echo "YOOO 1"
-ls -l
-echo "YOOO 1.5"
 cd mypy
-echo "YOOO 2"
 PYTHONPATH=../mypyc MYPYC_OPT_LEVEL=0 python3 setup.py --use-mypyc bdist_wheel
-echo "YOOO 3"
 mv dist ../wheelhouse
