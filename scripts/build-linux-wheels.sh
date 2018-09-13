7114de7a9cff7b0c70a8d0a94c74046453ac63d4
Michael J. Sullivan <sully@msully.net>
2018-09-13 14:35:34 -0700

    Do all the builds

--- scripts/build-linux-wheels.sh
+++ scripts/build-linux-wheels.sh
@@ -7,11 +7,10 @@ cd /io/mypy
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    #  if [ $(echo "${PYBIN}" | grep -o '[[:digit:]][[:digit:]]' | head -n 1) -ge 35 ]; then
-    if [ $(echo "${PYBIN}" | grep -o '[[:digit:]][[:digit:]]' | head -n 1) -ge 37 ]; then
+    if [ $(echo "${PYBIN}" | grep -o '[[:digit:]][[:digit:]]' | head -n 1) -ge 35 ]; then
 	# only builds on Python 3.5 and newer
 	"${PYBIN}/pip3" install -r /io/mypyc/external/mypy/test-requirements.txt
-	PYTHONPATH=/io/mypyc CC=/opt/llvm/bin/clang MYPYC_OPT_LEVEL=0 "${PYBIN}/python3" setup.py --use-mypyc bdist_wheel
+	PYTHONPATH=/io/mypyc CC=/opt/llvm/bin/clang MYPYC_OPT_LEVEL=3 "${PYBIN}/python3" setup.py --use-mypyc bdist_wheel
   fi
 done
 
