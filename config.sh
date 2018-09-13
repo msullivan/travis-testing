18ea19fdcf6c343327ed2cf71b60e6ed435e320a
Michael J. Sullivan <sully@msully.net>
2018-09-13 13:42:12 -0700

    Go back to something that worked

--- config.sh
+++ config.sh
@@ -1,14 +1,5 @@
 function pre_build {
     pip install -r mypy/test-requirements.txt
-    export MYPY_USE_MYPYC=1
-    export MYPYC_OPT_LEVEL=0
-    export PYTHONPATH="$PWD/mypyc:$PYTHONPATH"
-    if [ -n "$IS_OSX" ]; then
-	# Unpack a modern clang version
-	(cd / && curl -L https://github.com/msullivan/travis-testing/releases/download/llvm/llvm-centos-5.tar.gz | tar xzf -)
-	export CC=/opt/llvm/bin/clang
-    fi
-
 }
 
 function run_tests {
